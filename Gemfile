8d02f1a5c5dfcb883559c8ee043cf7caaa0bf088
Alexey3934 <gorrus39@mial.ru>
2023-06-03 13:20:23 +0200

    q

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+# gem "rails"
+
+gem 'json'
+gem 'net-http-persistent' # , '~> 2.9'
+gem 'sqlite3' # gem для БД
+gem 'telegram-bot-ruby', '~> 1.0'
---
+++ start.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'telegram/bot'
+
+token = ENV['T_BOT_5'] # 'YOUR_TELEGRAM_BOT_API_TOKEN'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      30.times do
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      end
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
